093fa205326501349df46737a150b33a79293d98
mick19880904 <79927574+mick19880904@users.noreply.github.com>
2021-04-11 14:42:58 +0800

    Add files via upload

---
+++ TQC+ 程式語言Python 107 數值計算.py
@@ -0,0 +1,10 @@
+x = ((eval(input())),(eval(input())),(eval(input())),(eval(input())),(eval(input())))
+# for i in x:
+#     print(i,end=" ")
+print(x[0],x[1],x[2],x[3],x[4])
+# print()
+print("Sum = %.1f"%(sum(x)))
+print("Average = %.1f"%(sum(x)/5))
+
+#11行把它標註掉，改成使用9/10/12 顯示結果也是一樣
+#但只有去掉9/10/12的答案才是他要的 所以錯的地方也很奇怪==
